86df9b4164edf69e65ee1dd42ccc4e331b5fad79
leebib1 <leeunji9807@gmail.com>
2023-07-14 09:02:31 +0900

    lombok 미설치 에러 해결

--- spring/src/main/java/com/bs/spring/config/BeanTestConfiguration.java
+++ spring/src/main/java/com/bs/spring/config/BeanTestConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.Order;
 
 import com.bs.spring.beantest.Animal;
